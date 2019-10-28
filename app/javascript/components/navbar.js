9502d170d9031bb6326a413727243080f4182af2
lux lux <luischn94@gmail.com>
2019-10-27 22:21:08 -0300

    iso image fixed

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lausol');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollBy >= window.innerHeight) {
+      if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-lausol-white');
       } else {
         navbar.classList.remove('navbar-lausol-white');
