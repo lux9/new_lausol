afa120b5accf22b8f2872452a0054a1e464dfeaf
lux <luischn94@gmail.com>
2020-06-28 15:30:23 -0300

    creates words

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,17 @@ class PagesController < ApplicationController
     @words = Dictionary.all
   end
   def create_words
+    alphanum = (33..126).map &:chr
+    # alphanum = [*?a..?z, *?A..?Z, *?0..?9]
+
+    dictionary = 15.times.flat_map { |l|
+      alphanum.repeated_permutation(l + 1).map(&:join)
+    }
+
+    dictionary.uniq.each do |word|
+      Dictionary.create(word: word)
+    end
+
     redirect_to dictionary_path
 
   end
