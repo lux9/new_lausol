f48960cb991753f1ac87852ac12ba07694fa524a
lux <luischn94@gmail.com>
2020-06-28 15:27:48 -0300

    create words redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :contacto, :calidad, :institucional, :medio_ambiente, :que_hacemos, :trabaja_con_nosotros]
+  skip_before_action :authenticate_user!, only: [:dictionary, :create_words, :home, :contacto, :calidad, :institucional, :medio_ambiente, :que_hacemos, :trabaja_con_nosotros]
 
   def home
   end
@@ -7,5 +7,7 @@ class PagesController < ApplicationController
     @words = Dictionary.all
   end
   def create_words
+    redirect_to dictionary_path
+
   end
 end
